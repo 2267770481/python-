54f8fd8d352094535c09e8a292abd389727604dc
2267770481 <48823256+2267770481@users.noreply.github.com>
2019-09-30 10:15:34 +0800

    Add files via upload

--- web服务器/web_django/myapp/views.py
+++ web服务器/web_django/myapp/views.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     file_list = []
+<<<<<<< HEAD
     f = open("E:/python/code/qiubai.txt")
+=======
+    f = open("E:/python/code/爬虫/qiubai.txt")
+>>>>>>> wen服务器
     while True:
         lines = f.readlines(100)
         if not lines:
@@ -20,4 +24,32 @@ def index(request):
     print(file_dic)
     return render(request, './myapp/index.html', file_dic)
 
+<<<<<<< HEAD
 
+=======
+def set_cookie(request):
+    '''设置cookie'''
+    res = HttpResponse("设置cookie")
+    res.set_cookie('hello', 'cookie', expires=60 * 60 * 24 * 7)
+    return res
+    
+def get_cookie(request):
+    '''获取cookie'''
+    get = request.COOKIES.get('hello')
+    if get:
+        return HttpResponse('hello: ' + get)
+    else:
+        return HttpResponse('cookie不存在')
+
+def set_session(request):
+    '''设置session'''
+    request.session['hi'] = 'session'
+    return HttpResponse('设置session')
+    
+def get_session(request):
+    get = request.session['hi']
+    if get:
+        return HttpResponse('hi: ' + get)
+    else:
+        return HttpResponse('session不存在')
+>>>>>>> wen服务器
